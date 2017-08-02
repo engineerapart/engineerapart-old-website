942fe3f1fe00d0df18e5126d356396397728c085
Kiky Tangerine <kiky@engineerapart.com>
2017-08-02 01:24:25 -0700

    Hooked emailjs to contactus form

--- js/new-age.js
+++ js/new-age.js
@@ -1,5 +1,5 @@
 (function ($) {
-  "use strict"; // Start of use strict
+  'use strict'; // Start of use strict
 
   // jQuery for page scrolling feature - requires jQuery Easing plugin
   $('a.page-scroll').bind('click', function (event) {
@@ -23,12 +23,12 @@
 
   // Offset for Main Navigation
   $('#mainNav').affix({ offset: { top: 50 } });
-  $('#mainNav').on('affix.bs.affix', function(){
+  $('#mainNav').on('affix.bs.affix', function () {
     // Occurs *after* the affix has been applied. Swap out the image for the colored one.
     $('#header-logo').addClass('in');
     $('#header-logo-mono').removeClass('in');
   });
-  $('#mainNav').on('affix-top.bs.affix', function(){
+  $('#mainNav').on('affix-top.bs.affix', function () {
     // Occurs *after* the affix has been removed. Swap out the image for the monochrome one.
     $('#header-logo').removeClass('in');
     $('#header-logo-mono').addClass('in');
@@ -36,8 +36,9 @@
 
   // Form button handler
   var isDetailedForm = false;
+  emailjs.init('user_58tWRHJDNJaH4a2Av20ih');
 
-  var handleToggle = function(target, other, showDetailed) {
+  var handleToggle = function (target, other, showDetailed) {
     target.removeClass('btn-default');
     target.addClass('btn-primary');
 
@@ -53,14 +54,42 @@
     }
   }
 
-  $('#simple-form').click(function(event) {
+  $('#simple-form').click(function (event) {
     event.preventDefault();
     handleToggle($(this), $('#detailed-form'), false);
+    $('#project-type').prop('selectedIndex', 0);
+    $('#budget').prop('selectedIndex', 0);
   });
-  
+
   $('#detailed-form').click(function (event) {
     event.preventDefault();
     handleToggle($(this), $('#simple-form'), true);
+    $('#project-type').prop('selectedIndex', 1);
+    $('#budget').prop('selectedIndex', 1);
   });
 
+  $('form#contact-us').submit(function (event) {
+    event.preventDefault();
+    var contactUsForm = $(this);
+
+    // Prevent double-clicking
+    contactUsForm.find('button.submit').prop('disabled', true);
+    contactUsForm.find('button.submit').text('Submitting...');
+
+    var service_id = 'default_service';
+    var template_id = 'new_customer_request';
+    
+    emailjs.sendForm(service_id, template_id, 'contact-us')
+      .then(function () {
+        $.notify('Thank you for your message!\n We will get back to you shortly.', 'success');
+        contactUsForm.find('button.submit').prop('disabled', false);
+        contactUsForm.find('button.submit').text('Submit');
+        contactUsForm.trigger('reset');
+      }, function (err) {
+        console.log(err);
+        $.notify('Oops, something went wrong, this is probably our bad.\n Please email us directly at info@engineerapart.com.', 'error');
+        contactUsForm.find('button.submit').prop('disabled', false);
+        contactUsForm.find('button.submit').text('Submit');
+      });
+  });
 })(jQuery); // End of use strict
